8be4bf6adac326273227e33ce7bd93a11ad8b287
David Stuber <rlzdavid20@gmail.com>
2024-06-10 17:44:01 +0200

    Create-Poll component: Created functions for add options, delete options and clear

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,8 +7,25 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [''];
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        return $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        return $this->options = array_values($this->options);
+    }
+
+    public function clearOption($index)
+    {
+        return $this->options[$index] = '';
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -3,5 +3,22 @@
         <label>Poll Title</label>
         <input type="text" wire:model.live="title" />
         Current Title: {{ $title }}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption()">Add Option</button>
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="mb-3">
+                    <label>Option {{ $index+1 }}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model.live="options.{{ $index }}">
+                        <button class="btn" wire:click.prevent="clearOption({{ $index }})">Clear</button>
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
